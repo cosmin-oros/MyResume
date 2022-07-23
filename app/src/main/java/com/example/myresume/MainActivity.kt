87b654539bd2cc0656ed3d86d7a122ac6c671706
cosmin-oros <oroscosmin09@gmail.com>
2022-07-23 19:26:52 +0300

    bottom nav bar

--- app/src/main/java/com/example/myresume/MainActivity.kt
+++ app/src/main/java/com/example/myresume/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.myresume.navigation.BottomNavItem
 import com.example.myresume.ui.theme.MyResumeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +21,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyResumeTheme {
+                val navController = rememberNavController()
 
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar(
+                            items = listOf(
+                                BottomNavItem(
+                                    name = "About",
+                                    route = "about",
+                                    icon = Icons.Filled
+                                ),
+                                BottomNavItem(
+                                    name = "Technologies",
+                                    route = "technologies",
+                                    icon = Icons.Filled
+                                ),
+                                BottomNavItem(
+                                    name = "Projects",
+                                    route = "projects",
+                                    icon = Icons.Filled
+                                ),
+                            ),
+                            navController = navController,
+                            onItemClick = {
+                                navController.navigate(it.route)
+                            }
+                            //change background default Color
+                        )
+                    }
+                ) {
+                    /*Navigation(navController = navController)*/
+                }
             }
         }
     }
--- app/src/main/java/com/example/myresume/navigation/Screen.kt
+++ app/src/main/java/com/example/myresume/navigation/Screen.kt
@@ -3,6 +3,7 @@ package com.example.myresume.navigation
 const val MACROS_ARGUMENT_KEY = "id"
 
 sealed class Screen(val route: String){
+    object SplashScreen: Screen("splash_screen")
     object AboutScreen: Screen("about")
     object TechnologiesScreen: Screen("technologies")
     object ProjectsScreen: Screen("projects")
