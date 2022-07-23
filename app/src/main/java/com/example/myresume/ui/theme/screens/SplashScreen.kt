58f611aa13eeffad379efa6a5a2ae2d4bf673260
cosmin-oros <oroscosmin09@gmail.com>
2022-07-23 19:38:13 +0300

    splash screen

---
+++ app/src/main/java/com/example/myresume/ui/theme/screens/SplashScreen.kt
@@ -0,0 +1,50 @@
+package com.example.myresume.ui.theme.screens
+
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavHostController
+import com.example.myresume.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavHostController){
+    val scale = remember{
+        Animatable(0f)
+
+    }
+    LaunchedEffect(key1 = true){
+        scale.animateTo(
+            targetValue = 0.3f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+        navController.navigate("about")
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        /*Image(
+            painter = painterResource(id = R.mipmap.ic_launcher_round),
+            contentDescription = "Logo",
+            modifier = Modifier.scale(10*scale.value)
+        )*/
+    }
+}
