3a45dc5bb021a6ab7de36eaa0ac1da78739b7f44
cosmin-oros <oroscosmin09@gmail.com>
2022-07-24 03:56:31 +0300

    screens

---
+++ app/src/main/java/com/example/myresume/ui/theme/screens/AboutScreen.kt
@@ -0,0 +1,9 @@
+package com.example.myresume.ui.theme.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun AboutScreen(navController: NavController) {
+
+}
---
+++ app/src/main/java/com/example/myresume/ui/theme/screens/ProjectsScreen.kt
@@ -0,0 +1,10 @@
+package com.example.myresume.ui.theme.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.myresume.navigation.Screen
+
+@Composable
+fun Screen.ProjectsScreen(navController: NavController) {
+
+}
