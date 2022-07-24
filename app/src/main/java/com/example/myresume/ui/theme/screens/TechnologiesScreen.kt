e11c352542dbfcbf4625df9b7f978afec9a577bc
cosmin-oros <oroscosmin09@gmail.com>
2022-07-24 17:07:41 +0300

    layout for technologies screen

--- app/src/main/java/com/example/myresume/ui/theme/screens/TechnologiesScreen.kt
+++ app/src/main/java/com/example/myresume/ui/theme/screens/TechnologiesScreen.kt
@@ -1,10 +1,31 @@
 package com.example.myresume.ui.theme.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.myresume.navigation.Screen
 
 @Composable
 fun Screen.TechnologiesScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(text = "Programming Languages")
 
+        Spacer(modifier = Modifier.size(16.dp))
+
+        Text(text = "Databases")
+
+        Spacer(modifier = Modifier.size(16.dp))
+
+        Text(text = "Tools")
+
+        Spacer(modifier = Modifier.size(16.dp))
+    }
 }
