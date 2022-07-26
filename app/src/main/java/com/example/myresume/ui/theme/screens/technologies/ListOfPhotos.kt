980aaf54081963ff88760f1ed3ebaa54b7ab4d02
cosmin-oros <oroscosmin09@gmail.com>
2022-07-26 13:56:34 +0300

    list of photos text

--- app/src/main/java/com/example/myresume/ui/theme/screens/technologies/ListOfPhotos.kt
+++ app/src/main/java/com/example/myresume/ui/theme/screens/technologies/ListOfPhotos.kt
@@ -6,30 +6,48 @@ object ListOfPhotos{
     fun getData(): ArrayList<PhotoData>{
         return arrayListOf(
             PhotoData(
+                title = "Kotlin",
+                text = "Android development, Backend, OOP",
                 imageResourceId = R.drawable.kotlin
             ),
             PhotoData(
+                title = "C",
+                text = "Data Structures and Algorithms",
                 imageResourceId = R.drawable.c
             ),
             PhotoData(
+                title = "C++",
+                text = "Data Structures and Algorithms, OOP",
                 imageResourceId = R.drawable.cpp
             ),
             PhotoData(
+                title = "Java",
+                text = "OOP, GUI, Games",
                 imageResourceId = R.drawable.java
             ),
             PhotoData(
+                title = "Python",
+                text = "ComputerVision (AI), Graphics, Games, OOP",
                 imageResourceId = R.drawable.python
             ),
             PhotoData(
+                title = "C#",
+                text = "Desktop Applications, OOP",
                 imageResourceId = R.drawable.csharp
             ),
             PhotoData(
+                title = "Matlab",
+                text = "Matrix manipulations, plotting of functions and data",
                 imageResourceId = R.drawable.matlab
             ),
             PhotoData(
+                title = "Dart",
+                text = "Cross platform mobile development (Flutter)",
                 imageResourceId = R.drawable.dart
             ),
             PhotoData(
+                title = "Bash",
+                text = "Command line work, security scripts",
                 imageResourceId = R.drawable.bash
             )
 
--- app/src/main/java/com/example/myresume/ui/theme/screens/technologies/PhotoData.kt
+++ app/src/main/java/com/example/myresume/ui/theme/screens/technologies/PhotoData.kt
@@ -1,6 +1,7 @@
 package com.example.myresume.ui.theme.screens.technologies
 
 data class PhotoData(
-    val text: String = "",
+    val title: String,
+    val text: String,
     val imageResourceId: Int
 )
