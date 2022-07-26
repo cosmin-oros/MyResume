203938ce162a7668840b7d7f95653a990e1978af
cosmin-oros <oroscosmin09@gmail.com>
2022-07-26 09:02:15 +0300

    technologies screen update

--- app/src/main/java/com/example/myresume/ui/theme/screens/TechnologiesScreen.kt
+++ app/src/main/java/com/example/myresume/ui/theme/screens/TechnologiesScreen.kt
@@ -1,9 +1,12 @@
 package com.example.myresume.ui.theme.screens
 
+import android.annotation.SuppressLint
 import android.provider.ContactsContract
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -12,9 +15,16 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.myresume.ImageCard
 import com.example.myresume.navigation.Screen
+import com.example.myresume.ui.theme.screens.technologies.ListOfPhotos
+import com.example.myresume.ui.theme.screens.technologies.PhotoData
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun Screen.TechnologiesScreen(navController: NavController) {
+    val photosList = ListOfPhotos.getData()
+    var counter = 0
+    var photo = mutableStateOf(getPhoto(photosList, counter))
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -26,6 +36,14 @@ fun Screen.TechnologiesScreen(navController: NavController) {
             // here insert photos of the programming languages
             //sort them by how much I've used them and their use case
 
+            var painter = painterResource(id = photo.value.imageResourceId)
+
+            //kotlin
+            Image(painter = painter, contentDescription = "")
+            counter++
+            photo.value = getPhoto(photosList, counter)
+
+            Spacer(modifier = Modifier.size(8.dp))
 
 
         }
@@ -47,4 +65,8 @@ fun Screen.TechnologiesScreen(navController: NavController) {
         }
 
     }
+}
+
+fun getPhoto(photosList: List<PhotoData>, counter: Int): PhotoData{
+    return photosList[counter]
 }
