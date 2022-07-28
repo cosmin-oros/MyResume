8cce64b91a79d1ba52df6051f55f927ca39aeaa6
cosmin-oros <oroscosmin09@gmail.com>
2022-07-28 11:12:40 +0300

    about screen

--- app/src/main/java/com/example/myresume/ui/theme/screens/AboutScreen.kt
+++ app/src/main/java/com/example/myresume/ui/theme/screens/AboutScreen.kt
@@ -5,5 +5,8 @@ import androidx.navigation.NavController
 
 @Composable
 fun AboutScreen(navController: NavController) {
+    //insert like in the lazycolumn card the photo on the left
+    // on the right the name and description etc
+    //below contact and more
 
 }
